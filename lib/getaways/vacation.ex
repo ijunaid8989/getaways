320ab76d7d8e7c0666732004e12a69e50318c171
Junaid Farooq <junaid@evercam.io>
2020-11-22 17:37:26 +0500

    Added vacatoin helper methods.

--- lib/getaways/vacation.ex
+++ lib/getaways/vacation.ex
@@ -6,7 +6,71 @@ defmodule Getaways.Vacation do
   import Ecto.Query, warn: false
   alias Getaways.Repo
 
-  alias Getaways.Vacation.Place
+  alias Getaways.Vacation.{Place, Booking, Review}
+  alias Getaways.Accounts.User
+
+  def get_place_by_slug!(slug) do
+    Repo.get_by!(Place, slug: slug)
+  end
+
+  def list_places(criteria) do
+    query = from p in Place
+
+    Enum.reduce(criteria, query, fn
+      {:limit, limit}, query ->
+        from p in query, limit: ^limit
+
+      {:filter, filters}, query ->
+        filter_with(filters, query)
+
+      {:order, order}, query ->
+        from p in query, order_by: [{^order, :id}]
+    end)
+    |> Repo.all()
+  end
+
+  defp filter_with(filters, query) do
+    Enum.reduce(filters, query, fn
+      {:matching, term}, query ->
+        pattern = "%#{term}"
+
+        from q in query,
+          where:
+            ilike(q.name, ^pattern) or
+              ilike(q.description, ^pattern) or
+              ilike(q.location, ^pattern)
+
+      {:pet_friendly, value}, query ->
+        from q in query, where: q.pet_friendly == ^value
+
+      {:pool, value}, query ->
+        from q in query, where: q.pool == ^value
+
+      {:wifi, value}, query ->
+        from q in query, where: q.wifi == ^value
+
+      {:guest_count, count}, query ->
+        from q in query, where: q.max_guests == ^count
+
+      {:available_between, %{start_date: start_date, end_date: end_date}}, query ->
+        available_between(query, start_date, end_date)
+    end)
+  end
+
+  defp available_between(query, start_date, end_date) do
+    from place in query,
+      left_join: booking in Booking,
+      on:
+        booking.place_id == place.id and
+          fragment(
+            "(?, ?) OVERLAPS (?, ? + INTERVAL '1' DAY)",
+            booking.start_date,
+            booking.end_date,
+            type(^start_date, :date),
+            type(^end_date, :date)
+          ),
+      where: is_nil(booking.place_id)
+  end
 
   @doc """
   Returns the list of places.
@@ -145,10 +209,33 @@ defmodule Getaways.Vacation do
       {:error, %Ecto.Changeset{}}
 
   """
-  def create_booking(attrs \\ %{}) do
+  def create_booking(%User{} = user, attrs) do
     %Booking{}
     |> Booking.changeset(attrs)
+    |> Ecto.Changeset.put_assoc(:user, user)
     |> Repo.insert()
+  end
+
+  def cancel_booking(%Booking{} = booking) do
+    booking
+    |> Booking.cancel_changeset(%{state: "canceled"})
+    |> Repo.update()
+  end
+
+  def datasource() do
+    Dataloader.Ecto.new(Repo, query: &query/2)
+  end
+
+  def query(Booking, %{limit: limit, scope: :place}) do
+    Booking
+    |> where(state: "reserved")
+    |> order_by(asc: :start_date)
+    |> limit(^limit)
+  end
+
+  def query(Booking, %{scope: :user}) do
+    Booking
+    |> order_by(asc: :start_date)
   end
 
   @doc """
