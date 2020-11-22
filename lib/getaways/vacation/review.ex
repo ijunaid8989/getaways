cf7cf164d85868018e02eaa4300c409afd786257
Junaid Farooq <junaid@evercam.io>
2020-11-22 12:51:57 +0500

    Added Getaways.Vacation.Review.

---
+++ lib/getaways/vacation/review.ex
@@ -0,0 +1,25 @@
+defmodule Getaways.Vacation.Review do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "reviews" do
+    field :comment, :string
+    field :rating, :integer
+
+    belongs_to :place, Getaways.Vacation.Place
+    belongs_to :user, Getaways.Accounts.User
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(review, attrs) do
+    required_fields = [:rating, :comment, :place_id]
+
+    review
+    |> cast(attrs, required_fields)
+    |> validate_required(required_fields)
+    |> assoc_constraint(:place)
+    |> assoc_constraint(:user)
+  end
+end
